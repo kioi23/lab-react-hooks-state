dc41b6d267ada3859ad9c926cbaa86a58408164d
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 14:05:22 +0300

    refactor ProductList component to use props and remove sample data

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import ProductCard from "./ProductCard";
 
-// Sample product data (for display purposes only)
-export const sampleProducts = [
-  { id: 1, name: 'Apple', price: '$1.00', category: 'Fruits', inStock: true },
-  { id: 2, name: 'Milk', price: '$2.50', category: 'Dairy', inStock: false }
-]
+function ProductList({ products, handleAddToCart }) {
 
-const ProductList = () => {
   return (
     <div>
-      <h2>Available Products</h2>
-
-      {/* TODO: Filter sample data using selected category */}
-      {sampleProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+          handleAddToCart={handleAddToCart}
+        />
       ))}
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
