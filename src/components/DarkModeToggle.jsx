1440056643a3e16f7dc3a195d8eaf23e97385955
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 14:00:25 +0300

    implement dark mode toggle functionality in DarkModeToggle component

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
-const DarkModeToggle = () => {
+import React from "react";
+function DarkModeToggle({ darkMode, setDarkMode }) {
   // TODO: Implement dark mode toggle logic
 
+  function handleClick() {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <button>Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}</button>
-  )
+    <button onClick={handleClick}>
+      {darkMode
+        ? "Toggle Light Mode"
+        : "Toggle Dark Mode"}
+        Toggle Dark Mode {/* TODO: Update this text from Dark to Light dynamically */}
+    </button>
+  );
 }
 
-export default DarkModeToggle
+export default DarkModeToggle;
